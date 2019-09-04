e1648097abad834ea9299a39579472451c099fa3
RohanGarg26 <garg.rohan26@gmail.com>
2019-09-04 19:58:28 +0530

    Added buttons to dynamically add ingridients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,27 @@ import classes from './Burger.module.css'
 import BurgerIngridient from './BurgerIngrideient/BurgerIngridient'
 
 const burger = (props) => {
-  return(
+  // console.log(Object.keys(props.ingridients))
+  let transformedIngridients = Object.keys(props.ingridients)
+    .map(igKey => {
+      // console.log(igKey)
+      // console.log([...Array(props.ingridients[igKey])].length)
+      return [...Array(props.ingridients[igKey])]
+        .map((_, i) => {
+          return <BurgerIngridient key={igKey + i} type={igKey} />
+        })
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+    if(transformedIngridients.length === 0){
+      transformedIngridients = <p>Please start adding ingridients.</p>
+    }
+  return (
     <div className={classes.Burger}>
-      <BurgerIngridient type="bread-top"/>
-      <BurgerIngridient type="cheese"/>
-      <BurgerIngridient type="meat"/>
-      <BurgerIngridient type="bread-bottom"/>
+      <BurgerIngridient type="bread-top" />
+      {transformedIngridients}
+      <BurgerIngridient type="bread-bottom" />
     </div>
   )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,53 @@ import React, { Component } from 'react'
 
 import Auxiliary from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/BuildControls/BuildControls'
+
+const INGRIDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
+  state = {
+    ingridients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  }
+
+  addIngridientHandler = (type) => {
+    const oldCount = this.state.ingridients[type];
+    // console.log(this.state.ingridients[type]);
+    const updateIngridients = {
+      ...this.state.Ingridients
+    }
+    const updatedCounted = oldCount + 1;
+    updateIngridients[type] = updatedCounted
+    const priceAddition = INGRIDIENT_PRICES[type]
+    const oldPrice= this.state.totalPrice
+    const newPrice = oldPrice + priceAddition
+    this.setState({
+      totalPrice: newPrice,
+      ingridients: updateIngridients
+    })
+  }
+
+  removeIngridientHandler = (type) => {
+
+  }
+
   render() {
     return (
       <Auxiliary>
-        <Burger />
-        <div>Build Controls</div>
+        <Burger ingridients={this.state.ingridients} />
+        <BuildControls 
+          ingridientAdded={this.addIngridientHandler}/>
       </Auxiliary>
     )
   }
